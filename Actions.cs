6316b2f3a4c419658145d279c4e3653cf0fecd29
UPJM <44162070+UPJM@users.noreply.github.com>
2022-02-10 09:08:50 +0000

    Add files via upload

---
+++ Actions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FizzBuzz
+{
+    //class that contains actions
+    public class Actions
+    {
+
+        // Accepts a list and returns the
+        // required outputs
+        public static void DoFizzBuzz(List<int> data)
+        {
+            if (data != null)
+            {
+
+                foreach (var num in data)
+                {
+                    if (num % 3 == 0 && num % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (num % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (num % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(num);
+                    }
+                }
+            }
+            else {
+                Console.WriteLine("List Data Empty.");
+
+
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FizzBuzz
+{
+    class program
+    {
+
+        public static void Main(string[] args)
+        {
+            var data = Data.DataList(1, 100);
+            Actions.DoFizzBuzz(data);
+
+        }
+    }
+
+ }
+
+
+
